6ef44d244c05b0043f0659a02fe9cd3b41d93517
Carlos H. Campello <carlos@qualinfo.com.br>
2023-10-19 09:26:24 -0300

    First commit

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# camelot-roundtable-list
+# hercules-olympic-circle-list
 
-This GitHub repo contains the protocols list of Round Table members.
+This GitHub repo contains the protocols list of Olympic Circle members.
 
 ## Adding your protocol
 
-To add your protocol to the Round Table list, you should create your entry and submit a PR with it. 
+To add your protocol to the Olympic Circle list, you should create your entry and submit a PR with it. 
 
 **1. Fork this repo**
 
@@ -13,7 +13,7 @@ To add your protocol to the Round Table list, you should create your entry and s
 **3. Provide the following required information for your protocol.**
 
 - chainId (number)
-- id (string): must be unique, will be used as a slug on https://app.camelot.exchange/protocols/id
+- id (string): must be unique, will be used as a slug on https://app.hercules.exchange/protocols/id
 - logo (string): url, small format square img, will be used in the project list, svg preferred
 - cover (string): url, large format img, will be used on the main protocol page as a cover image, svg preferred
 - tagline (string): one short sentence
--- src/buildList.js
+++ src/buildList.js
@@ -1,17 +1,17 @@
-const arbitrumNova = require("./protocols/arbitrum-nova.json");
-const arbitrumOne = require("./protocols/arbitrum-one.json");
+const metisAndromeda = require("./protocols/metis-andromeda.json");
+const metisGoerli = require("./protocols/metis-goerli.json");
 
 module.exports = function buildList() {
-    return {
-        name: "Camelot RoundTable",
-        timestamp: new Date().toISOString(),
-        protocols: [...arbitrumNova, ...arbitrumOne]
-            // sort them by symbol for easy readability
-            .sort((p1, p2) => {
-                if (p1.chainId === p2.chainId) {
-                    return p1.name.toLowerCase() < p2.name.toLowerCase() ? -1 : 1;
-                }
-                return p1.chainId < p2.chainId ? -1 : 1;
-            }),
-    };
+  return {
+    name: "Hercules Olympic Circle List",
+    timestamp: new Date().toISOString(),
+    protocols: [...metisAndromeda, ...metisGoerli]
+      // sort them by symbol for easy readability
+      .sort((p1, p2) => {
+        if (p1.chainId === p2.chainId) {
+          return p1.name.toLowerCase() < p2.name.toLowerCase() ? -1 : 1;
+        }
+        return p1.chainId < p2.chainId ? -1 : 1;
+      }),
+  };
 };
