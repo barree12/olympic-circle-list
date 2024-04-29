3a15327aa605698bd8f1d0b65b55141f5a902348
buroz <hey@buroz.dev>
2024-04-29 13:36:52 +0300

    chains fix

--- src/buildList.js
+++ src/buildList.js
@@ -1,11 +1,10 @@
 const metisAndromeda = require("./protocols/metis-andromeda.json");
-const metisGoerli = require("./protocols/metis-goerli.json");
 
 module.exports = function buildList() {
   return {
     name: "Hercules Olympic Circle List",
     timestamp: new Date().toISOString(),
-    protocols: [...metisAndromeda, ...metisGoerli]
+    protocols: [...metisAndromeda]
       // sort them by symbol for easy readability
       .sort((p1, p2) => {
         if (p1.chainId === p2.chainId) {
